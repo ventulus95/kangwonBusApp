c3306446cee72a96163a84910620014dcc8074fb
이창섭 <ventulus95@gmail.com>
2020-02-21 00:08:42 +0900

    React & SpringBoot 첫 커밋

---
+++ src/main/java/com/ventulus95/reactspringboot/ReactspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ventulus95.reactspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ventulus95/reactspringboot/ReactspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventulus95.reactspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
